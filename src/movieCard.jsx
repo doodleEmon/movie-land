da3f68e921a2beac75bcc5f1518c3533888c5895
Emon <150001421+doodleEmon@users.noreply.github.com>
2025-01-05 02:17:40 +0600

    Update movieCard.jsx

--- src/movieCard.jsx
+++ src/movieCard.jsx
@@ -6,9 +6,16 @@ const MovieCard = ({ movie }) => {
         src={movie.Poster !== "N/A" ? movie.Poster : "https://placehold.co/400"}
         alt={movie.Title}
       />
-      <div className="px-4 py-2 bg-gray-700">
-        <h3 className="text-white">{movie.Type}</h3>
-        <h3 className="text-white">{movie.Title}</h3>
+      <div className="px-4 py-2 bg-gray-700 flex justify-between">
+        <div>
+          <h3 className="text-white">{movie.Type}</h3>
+          <h3 className="text-white">{movie.Title}</h3>
+        </div>
+        <div>
+          <h3 className="text-gray-700 bg-white px-2 rounded-md font-medium">
+            {movie.Year}
+          </h3>
+        </div>
       </div>
     </div>
   );
